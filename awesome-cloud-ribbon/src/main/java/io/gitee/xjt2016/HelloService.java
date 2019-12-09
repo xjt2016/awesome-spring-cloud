dcc72022b0379d03dd7cf15a720e3209aefae7c8
xjt2016 <xiongjinteng@gmail.com>
2019-12-09 11:43:15 +0800

    update

--- awesome-cloud-ribbon/src/main/java/io/gitee/xjt2016/HelloService.java
+++ awesome-cloud-ribbon/src/main/java/io/gitee/xjt2016/HelloService.java
@@ -1,5 +1,6 @@
 package io.gitee.xjt2016;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,8 +12,14 @@ public class HelloService {
     @Resource
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
-        return restTemplate.getForObject("http://server-hi/hi?name=" + name, String.class);
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
     }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
+    }
+
 }
 

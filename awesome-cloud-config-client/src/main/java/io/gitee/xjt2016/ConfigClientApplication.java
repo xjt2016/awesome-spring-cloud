4588ea9ed815c40653fe7384e50bcee912c4b75f
xjt2016 <xiongjinteng@gmail.com>
2019-12-10 08:45:41 +0800

    update:

---
+++ awesome-cloud-config-client/src/main/java/io/gitee/xjt2016/ConfigClientApplication.java
@@ -0,0 +1,28 @@
+package io.gitee.xjt2016;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+
+        return foo;
+    }
+}
